73e3eceb702be5e9b8bc8aabb115ce5dc2d43c67
Boldi Z <zboldi@gmail.com>
2021-04-04 21:39:59 +0200

    Hide Cursor

--- src/index.js
+++ src/index.js
@@ -12,6 +12,11 @@ function displayTime() {
     timeHoursElement.innerText = String(timeHours).padStart(2, "0")
     timeMinutesElement.innerText = String(timeMinutes).padStart(2, "0")
     timeSecondsElement.innerText = String(timeSeconds).padStart(2, "0")
+
+    // Move sursor slightly to hide it
+    let cursor = document.getElementById("cursor");
+    cursor.style.left = 10 + "px";
+    cursor.style.top = 10 + "px";
 }
 
 setInterval(displayTime, 100)
