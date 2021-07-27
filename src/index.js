65884c535cd27a919f5a143ac24ad5c5f8f04cb2
Boldizsar Zopcsak <boldizsar.zopcsak@tum.de>
2021-07-27 21:46:57 +0200

    Remove seconds and add second row of text

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ function displayTime() {
     let time = new Date()
     let timeHours= time.getHours()
     let timeMinutes = time.getMinutes()
-    let timeSeconds = time.getSeconds()
 
     const timeHoursElement = document.getElementById('time-hours')
     const timeMinutesElement = document.getElementById('time-minutes')
-    const timeSecondsElement = document.getElementById('time-seconds')
     
     timeHoursElement.innerText = String(timeHours).padStart(2, "0")
     timeMinutesElement.innerText = String(timeMinutes).padStart(2, "0")
-    timeSecondsElement.innerText = String(timeSeconds).padStart(2, "0")
 }
 
 setInterval(displayTime, 100)
